d1cfaf06061f6cb92e669059632c9c4b0732fa57
Naman Bhalla <namanbhalla1998@gmail.com>
2020-06-30 21:03:31 +0530

    Initial Project

---
+++ src/main/java/dev/naman/onlinejudge/OnlinejudgeApplication.java
@@ -0,0 +1,13 @@
+package dev.naman.onlinejudge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinejudgeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlinejudgeApplication.class, args);
+    }
+
+}
