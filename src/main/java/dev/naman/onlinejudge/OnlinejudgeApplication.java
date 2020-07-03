a76e1d06c9ce5c487d924d20244243a5acdc40d3
SONU RAJ <srajsonu02@gmail.com>
2020-07-03 19:14:58 +0530

    Minor changes

--- src/main/java/dev/naman/onlinejudge/OnlinejudgeApplication.java
+++ src/main/java/dev/naman/onlinejudge/OnlinejudgeApplication.java
@@ -2,8 +2,9 @@ package dev.naman.onlinejudge;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class OnlinejudgeApplication {
 
     public static void main(String[] args) {
