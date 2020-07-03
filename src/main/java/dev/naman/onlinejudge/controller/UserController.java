f6cb43a210c591ee134147afec02e4c19189a33b
SONU RAJ <srajsonu02@gmail.com>
2020-07-03 19:40:25 +0530

    New Controller added

---
+++ src/main/java/dev/naman/onlinejudge/controller/UserController.java
@@ -0,0 +1,13 @@
+package dev.naman.onlinejudge.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @RequestMapping("/hello")
+    public String hello() {
+        return "Hello world!";
+    }
+}
